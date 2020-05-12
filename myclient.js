66f59546ced606e148a2a95707805f9f76f5fafe
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-05-12 15:44:46 -0400

    fixed myclient.js

--- myclient.js
+++ myclient.js
@@ -1,16 +1,7 @@
-/**
- * MYCLIENT.JS
- * an example web app using an ajax request to our API server which returns a JSON object 
- * 
- * When a user opens index.html it then loads and executes this JavaScript code, which reads 
- * the current logic level on the input ports, and displays that in the window, each in a div 
- * identified by the index "inputs_[port number]"
- */
-
 window.onload = function () {
   var url,
     i,
-    ports = [23, 25];  // the GPIO ports we will read
+    ports = [7];  // the GPIO ports we will read
 
   for (i in ports) {
     $('#input_' + ports[i]).html('loading port ' + ports[i] + ' value...');
